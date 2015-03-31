e0026319c2e29ce3c1ca7de30d6b2d8420cd71e9
somkiat <somkiat.p@gmail.com>
2015-03-31 17:32:36 +0700

    Demo 1

--- src/main/java/swtesting/FizzBuzz.java
+++ src/main/java/swtesting/FizzBuzz.java
@@ -2,8 +2,28 @@ package swtesting;
 
 public class FizzBuzz {
 
-    public String say(int i) {
-        return "1";
+    public String say(int number) {
+        String result = String.valueOf(number);
+        if (evaluateFizzBuzz(number)) {
+            result = "FizzBuzz";
+        } else if (evaluateFizz(number)) {
+            result = "Fizz";
+        } else if (evaluateBuzz(number)) {
+            result = "Buzz";
+        }
+        return result;
+    }
+
+    private boolean evaluateFizzBuzz(int number) {
+        return evaluateFizz(number) && evaluateBuzz(number);
+    }
+
+    private boolean evaluateFizz(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean evaluateBuzz(int number) {
+        return number % 5 == 0;
     }
 
 }
--- src/test/java/swtesting/FizzBuzzTest.java
+++ src/test/java/swtesting/FizzBuzzTest.java
@@ -5,12 +5,43 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+    
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    public void test() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+    public void shouldSayOneWhenNumberIsOne() {
         String actualResult = fizzBuzz.say(1);
         assertEquals("1", actualResult);
     }
-
+    
+    @Test
+    public void shouldSayTwoWhenNumberIsTwo() {
+        String actualResult = fizzBuzz.say(2);
+        assertEquals("2", actualResult);
+    }
+    
+    @Test
+    public void shouldSayFizzWhenNumberIsThree() {
+        String actualResult = fizzBuzz.say(3);
+        assertEquals("Fizz", actualResult);
+    }
+    
+    @Test
+    public void shouldSayFizzWhenNumberIsSix() {
+        String actualResult = fizzBuzz.say(6);
+        assertEquals("Fizz", actualResult);
+    }
+    
+    @Test
+    public void shouldSayBuzzWhenNumberIsFive() {
+        String actualResult = fizzBuzz.say(5);
+        assertEquals("Buzz", actualResult);
+    }
+    
+    @Test
+    public void shouldSayFizzBuzzWhenNumberIsFifteen() {
+        String actualResult = fizzBuzz.say(15);
+        assertEquals("FizzBuzz", actualResult);
+    }
+    
 }
